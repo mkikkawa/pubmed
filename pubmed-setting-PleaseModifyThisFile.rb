110b3bbbbfa7e458107f4774661664083e5b706e
Masahide Kikkawa <mkikkawa@gmail.com>
2019-08-21 15:16:49 +0900

    Add files via upload

---
+++ pubmed-setting-PleaseModifyThisFile.rb
@@ -0,0 +1,5 @@
+$pdf_dir =  "/path/to/your/pdf/directory"
+$download_dir = "/path/to/your/Download/directory"
+$my_email_address = "your@email.address"
+$ip_addresses_for_sync = /192\.168\.11\./
+$pubmed_max_n = 1000
